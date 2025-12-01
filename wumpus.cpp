8f91abea1c787246cadbb11b8a8c2fc3106ab617
DanielKruts <djkrutsick@icloud.com>
2025-12-01 12:03:21 -0500

    I added the bingus bongus to the wumpus.

--- wumpus.cpp
+++ wumpus.cpp
@@ -2,7 +2,22 @@
 #include <vector>
 using namespace std;
 
+//Defines our types of grid squares, default values are 0, can change the default values of squareType if need be
+enum squareType{
+    EMPTY,
+    MONEY,
+    STINK,
+    ENTRY,
+    WUMPUS
+};
+
+//Defines the structure of our grid, having whatever enumerated squareType and a value assigned to that square
+struct Grid{
+    squareType type;
+    int cost;
+};
+
 int main(){
-    vector<vector<int>> matrix;
+    
     return 0;
 }
